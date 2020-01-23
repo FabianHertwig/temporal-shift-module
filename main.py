c0f7db3c059f9da3e04cb3eef3a21b72d7e05add
Fabian Hertwig <fabian.hertwig@maibornwolff.de>
2020-01-23 14:20:01 +0100

    Remove everything that is not needed for an inference run

--- main.py
+++ main.py
@@ -1,378 +1,135 @@
-# Code for "TSM: Temporal Shift Module for Efficient Video Understanding"
-# arXiv:1811.08383
-# Ji Lin*, Chuang Gan, Song Han
-# {jilin, songhan}@mit.edu, ganchuang@csail.mit.edu
-
 import os
 import time
-import shutil
-import torch.nn.parallel
-import torch.backends.cudnn as cudnn
-import torch.optim
-from torch.nn.utils import clip_grad_norm_
 
-from ops.dataset import TSNDataSet
-from ops.models import TSN
-from ops.transforms import *
-from opts import parser
-from ops import dataset_config
-from ops.utils import AverageMeter, accuracy
-from ops.temporal_shift import make_temporal_pool
-
-from tensorboardX import SummaryWriter
-
-best_prec1 = 0
+import torch
+import torchvision
+import cv2
+from PIL import Image, ImageOps
+import numpy as np
 
 
-def main():
-    global args, best_prec1
-    args = parser.parse_args()
+from src.mobilenet_v2_tsm import MobileNetV2
+from src.image_processing import get_transform
+from src.gestures import Gestures
+from src.prediction_smoothing import PredictionSmoothing
 
-    num_class, args.train_list, args.val_list, args.root_path, prefix = dataset_config.return_dataset(args.dataset,
-                                                                                                      args.modality)
-    full_arch_name = args.arch
-    if args.shift:
-        full_arch_name += '_shift{}_{}'.format(args.shift_div, args.shift_place)
-    if args.temporal_pool:
-        full_arch_name += '_tpool'
-    args.store_name = '_'.join(
-        ['TSM', args.dataset, args.modality, full_arch_name, args.consensus_type, 'segment%d' % args.num_segments,
-         'e{}'.format(args.epochs)])
-    if args.pretrain != 'imagenet':
-        args.store_name += '_{}'.format(args.pretrain)
-    if args.lr_type != 'step':
-        args.store_name += '_{}'.format(args.lr_type)
-    if args.dense_sample:
-        args.store_name += '_dense'
-    if args.non_local > 0:
-        args.store_name += '_nl'
-    if args.suffix is not None:
-        args.store_name += '_{}'.format(args.suffix)
-    print('storing name: ' + args.store_name)
 
-    check_rootfolders()
+def load_model():
+    torch_module = MobileNetV2(n_class=27)
+    # checkpoint not downloaded
+    if not os.path.exists("mobilenetv2_jester_online.pth.tar"):
+        print('Downloading PyTorch checkpoint...')
+        import urllib.request
+        url = 'https://hanlab.mit.edu/projects/tsm/models/mobilenetv2_jester_online.pth.tar'
+        urllib.request.urlretrieve(url, './mobilenetv2_jester_online.pth.tar')
+    torch_module.load_state_dict(torch.load(
+        "mobilenetv2_jester_online.pth.tar"))
 
-    model = TSN(num_class, args.num_segments, args.modality,
-                base_model=args.arch,
-                consensus_type=args.consensus_type,
-                dropout=args.dropout,
-                img_feature_dim=args.img_feature_dim,
-                partial_bn=not args.no_partialbn,
-                pretrain=args.pretrain,
-                is_shift=args.shift, shift_div=args.shift_div, shift_place=args.shift_place,
-                fc_lr5=not (args.tune_from and args.dataset in args.tune_from),
-                temporal_pool=args.temporal_pool,
-                non_local=args.non_local)
+    return torch_module
 
-    crop_size = model.crop_size
-    scale_size = model.scale_size
-    input_mean = model.input_mean
-    input_std = model.input_std
-    policies = model.get_optim_policies()
-    train_augmentation = model.get_augmentation(flip=False if 'something' in args.dataset or 'jester' in args.dataset else True)
 
-    model = torch.nn.DataParallel(model, device_ids=args.gpus).cuda()
+def init_buffer():
+    return [torch.zeros([1, 3, 56, 56]),
+            torch.zeros([1, 4, 28, 28]),
+            torch.zeros([1, 4, 28, 28]),
+            torch.zeros([1, 8, 14, 14]),
+            torch.zeros([1, 8, 14, 14]), 
+            torch.zeros([1, 8, 14, 14]), 
+            torch.zeros([1, 12, 14, 14]), 
+            torch.zeros([1, 12, 14, 14]), 
+            torch.zeros([1, 20, 7, 7]), 
+            torch.zeros([1, 20, 7, 7])]
 
-    optimizer = torch.optim.SGD(policies,
-                                args.lr,
-                                momentum=args.momentum,
-                                weight_decay=args.weight_decay)
 
-    if args.resume:
-        if args.temporal_pool:  # early temporal pool so that we can load the state_dict
-            make_temporal_pool(model.module.base_model, args.num_segments)
-        if os.path.isfile(args.resume):
-            print(("=> loading checkpoint '{}'".format(args.resume)))
-            checkpoint = torch.load(args.resume)
-            args.start_epoch = checkpoint['epoch']
-            best_prec1 = checkpoint['best_prec1']
-            model.load_state_dict(checkpoint['state_dict'])
-            optimizer.load_state_dict(checkpoint['optimizer'])
-            print(("=> loaded checkpoint '{}' (epoch {})"
-                   .format(args.evaluate, checkpoint['epoch'])))
-        else:
-            print(("=> no checkpoint found at '{}'".format(args.resume)))
+def is_quit_key(key):
+    return key & 0xFF == ord('q') or key == 27
 
-    if args.tune_from:
-        print(("=> fine-tuning from '{}'".format(args.tune_from)))
-        sd = torch.load(args.tune_from)
-        sd = sd['state_dict']
-        model_dict = model.state_dict()
-        replace_dict = []
-        for k, v in sd.items():
-            if k not in model_dict and k.replace('.net', '') in model_dict:
-                print('=> Load after remove .net: ', k)
-                replace_dict.append((k, k.replace('.net', '')))
-        for k, v in model_dict.items():
-            if k not in sd and k.replace('.net', '') in sd:
-                print('=> Load after adding .net: ', k)
-                replace_dict.append((k.replace('.net', ''), k))
 
-        for k, k_new in replace_dict:
-            sd[k_new] = sd.pop(k)
-        keys1 = set(list(sd.keys()))
-        keys2 = set(list(model_dict.keys()))
-        set_diff = (keys1 - keys2) | (keys2 - keys1)
-        print('#### Notice: keys that failed to load: {}'.format(set_diff))
-        if args.dataset not in args.tune_from:  # new dataset
-            print('=> New dataset, do not load fc weights')
-            sd = {k: v for k, v in sd.items() if 'fc' not in k}
-        if args.modality == 'Flow' and 'Flow' not in args.tune_from:
-            sd = {k: v for k, v in sd.items() if 'conv1.weight' not in k}
-        model_dict.update(sd)
-        model.load_state_dict(model_dict)
+def is_fullscreen_key(key):
+    return key == ord('F') or key == ord('f')
 
-    if args.temporal_pool and not args.resume:
-        make_temporal_pool(model.module.base_model, args.num_segments)
 
-    cudnn.benchmark = True
-
-    # Data loading code
-    if args.modality != 'RGBDiff':
-        normalize = GroupNormalize(input_mean, input_std)
+def switch_fullscreen_mode(full_screen: bool, window_name: str):
+    if full_screen:
+        cv2.setWindowProperty(window_name, cv2.WND_PROP_FULLSCREEN,
+                              cv2.WINDOW_FULLSCREEN)
     else:
-        normalize = IdentityTransform()
-
-    if args.modality == 'RGB':
-        data_length = 1
-    elif args.modality in ['Flow', 'RGBDiff']:
-        data_length = 5
-
-    train_loader = torch.utils.data.DataLoader(
-        TSNDataSet(args.root_path, args.train_list, num_segments=args.num_segments,
-                   new_length=data_length,
-                   modality=args.modality,
-                   image_tmpl=prefix,
-                   transform=torchvision.transforms.Compose([
-                       train_augmentation,
-                       Stack(roll=(args.arch in ['BNInception', 'InceptionV3'])),
-                       ToTorchFormatTensor(div=(args.arch not in ['BNInception', 'InceptionV3'])),
-                       normalize,
-                   ]), dense_sample=args.dense_sample),
-        batch_size=args.batch_size, shuffle=True,
-        num_workers=args.workers, pin_memory=True,
-        drop_last=True)  # prevent something not % n_GPU
-
-    val_loader = torch.utils.data.DataLoader(
-        TSNDataSet(args.root_path, args.val_list, num_segments=args.num_segments,
-                   new_length=data_length,
-                   modality=args.modality,
-                   image_tmpl=prefix,
-                   random_shift=False,
-                   transform=torchvision.transforms.Compose([
-                       GroupScale(int(scale_size)),
-                       GroupCenterCrop(crop_size),
-                       Stack(roll=(args.arch in ['BNInception', 'InceptionV3'])),
-                       ToTorchFormatTensor(div=(args.arch not in ['BNInception', 'InceptionV3'])),
-                       normalize,
-                   ]), dense_sample=args.dense_sample),
-        batch_size=args.batch_size, shuffle=False,
-        num_workers=args.workers, pin_memory=True)
-
-    # define loss function (criterion) and optimizer
-    if args.loss_type == 'nll':
-        criterion = torch.nn.CrossEntropyLoss().cuda()
-    else:
-        raise ValueError("Unknown loss type")
-
-    for group in policies:
-        print(('group: {} has {} params, lr_mult: {}, decay_mult: {}'.format(
-            group['name'], len(group['params']), group['lr_mult'], group['decay_mult'])))
-
-    if args.evaluate:
-        validate(val_loader, model, criterion, 0)
-        return
-
-    log_training = open(os.path.join(args.root_log, args.store_name, 'log.csv'), 'w')
-    with open(os.path.join(args.root_log, args.store_name, 'args.txt'), 'w') as f:
-        f.write(str(args))
-    tf_writer = SummaryWriter(log_dir=os.path.join(args.root_log, args.store_name))
-    for epoch in range(args.start_epoch, args.epochs):
-        adjust_learning_rate(optimizer, epoch, args.lr_type, args.lr_steps)
-
-        # train for one epoch
-        train(train_loader, model, criterion, optimizer, epoch, log_training, tf_writer)
-
-        # evaluate on validation set
-        if (epoch + 1) % args.eval_freq == 0 or epoch == args.epochs - 1:
-            prec1 = validate(val_loader, model, criterion, epoch, log_training, tf_writer)
-
-            # remember best prec@1 and save checkpoint
-            is_best = prec1 > best_prec1
-            best_prec1 = max(prec1, best_prec1)
-            tf_writer.add_scalar('acc/test_top1_best', best_prec1, epoch)
-
-            output_best = 'Best Prec@1: %.3f\n' % (best_prec1)
-            print(output_best)
-            log_training.write(output_best + '\n')
-            log_training.flush()
-
-            save_checkpoint({
-                'epoch': epoch + 1,
-                'arch': args.arch,
-                'state_dict': model.state_dict(),
-                'optimizer': optimizer.state_dict(),
-                'best_prec1': best_prec1,
-            }, is_best)
+        cv2.setWindowProperty(window_name, cv2.WND_PROP_FULLSCREEN,
+                              cv2.WINDOW_NORMAL)
 
 
-def train(train_loader, model, criterion, optimizer, epoch, log, tf_writer):
-    batch_time = AverageMeter()
-    data_time = AverageMeter()
-    losses = AverageMeter()
-    top1 = AverageMeter()
-    top5 = AverageMeter()
-
-    if args.no_partialbn:
-        model.module.partialBN(False)
-    else:
-        model.module.partialBN(True)
-
-    # switch to train mode
-    model.train()
-
-    end = time.time()
-    for i, (input, target) in enumerate(train_loader):
-        # measure data loading time
-        data_time.update(time.time() - end)
-
-        target = target.cuda()
-        input_var = torch.autograd.Variable(input)
-        target_var = torch.autograd.Variable(target)
-
-        # compute output
-        output = model(input_var)
-        loss = criterion(output, target_var)
-
-        # measure accuracy and record loss
-        prec1, prec5 = accuracy(output.data, target, topk=(1, 5))
-        losses.update(loss.item(), input.size(0))
-        top1.update(prec1.item(), input.size(0))
-        top5.update(prec5.item(), input.size(0))
-
-        # compute gradient and do SGD step
-        loss.backward()
-
-        if args.clip_gradient is not None:
-            total_norm = clip_grad_norm_(model.parameters(), args.clip_gradient)
-
-        optimizer.step()
-        optimizer.zero_grad()
-
-        # measure elapsed time
-        batch_time.update(time.time() - end)
-        end = time.time()
-
-        if i % args.print_freq == 0:
-            output = ('Epoch: [{0}][{1}/{2}], lr: {lr:.5f}\t'
-                      'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
-                      'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
-                      'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
-                      'Prec@1 {top1.val:.3f} ({top1.avg:.3f})\t'
-                      'Prec@5 {top5.val:.3f} ({top5.avg:.3f})'.format(
-                epoch, i, len(train_loader), batch_time=batch_time,
-                data_time=data_time, loss=losses, top1=top1, top5=top5, lr=optimizer.param_groups[-1]['lr'] * 0.1))  # TODO
-            print(output)
-            log.write(output + '\n')
-            log.flush()
-
-    tf_writer.add_scalar('loss/train', losses.avg, epoch)
-    tf_writer.add_scalar('acc/train_top1', top1.avg, epoch)
-    tf_writer.add_scalar('acc/train_top5', top5.avg, epoch)
-    tf_writer.add_scalar('lr', optimizer.param_groups[-1]['lr'], epoch)
+def setup_window(window_name):
+    cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
+    cv2.resizeWindow(window_name, 640, 480)
+    cv2.moveWindow(window_name, 0, 0)
+    cv2.setWindowTitle(window_name, window_name)
 
 
-def validate(val_loader, model, criterion, epoch, log=None, tf_writer=None):
-    batch_time = AverageMeter()
-    losses = AverageMeter()
-    top1 = AverageMeter()
-    top5 = AverageMeter()
+def get_camera_capture(camera: int, width: int, height: int):
+    cap = cv2.VideoCapture(camera)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+    return cap
 
-    # switch to evaluate mode
+
+def add_label(img: np.array, gesture_name: str, frames_per_second: float):
+    height, width, _ = img.shape
+    label = np.zeros([height // 10, width, 3]).astype('uint8') + 255
+    cv2.putText(label, 'Prediction: ' + gesture_name,
+                (0, int(height / 16)),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.7, (0, 0, 0), 2)
+    cv2.putText(label, '{:.1f} Frames/s'.format(frames_per_second),
+                (width - 170, int(height / 16)),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.7, (0, 0, 0), 2)
+    img = np.concatenate((img, label), axis=0)
+    return img
+
+if __name__ == "__main__":
+    model = load_model()
     model.eval()
+    
+    cap = get_camera_capture(0, 320, 240)
 
-    end = time.time()
-    with torch.no_grad():
-        for i, (input, target) in enumerate(val_loader):
-            target = target.cuda()
+    full_screen = False
+    window_name = 'Video Gesture Recognition'
+    setup_window(window_name)
 
-            # compute output
-            output = model(input)
-            loss = criterion(output, target)
+    transform = get_transform()
+    shift_buffer = init_buffer()
+    gestures = Gestures()
+    prediction_smoothing = PredictionSmoothing(7)
 
-            # measure accuracy and record loss
-            prec1, prec5 = accuracy(output.data, target, topk=(1, 5))
+    while True:
+        time_start = time.time()
+        _, img = cap.read() 
 
-            losses.update(loss.item(), input.size(0))
-            top1.update(prec1.item(), input.size(0))
-            top5.update(prec5.item(), input.size(0))
+        with torch.no_grad():
+            image_transformed = transform([Image.fromarray(img).convert('RGB')])
+            input_transformed = image_transformed.view(1, 3, image_transformed.size(1), image_transformed.size(2))
+            predictions, *shift_buffer = model(input_transformed, *shift_buffer)
 
-            # measure elapsed time
-            batch_time.update(time.time() - end)
-            end = time.time()
+            _, prediction = predictions.max(1)
+            prediction = prediction.item()
+            prediction_smoothing.add_prediction(prediction)
+            smooth_prediction = prediction_smoothing.get_most_common_prediction()
 
-            if i % args.print_freq == 0:
-                output = ('Test: [{0}/{1}]\t'
-                          'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
-                          'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
-                          'Prec@1 {top1.val:.3f} ({top1.avg:.3f})\t'
-                          'Prec@5 {top5.val:.3f} ({top5.avg:.3f})'.format(
-                    i, len(val_loader), batch_time=batch_time, loss=losses,
-                    top1=top1, top5=top5))
-                print(output)
-                if log is not None:
-                    log.write(output + '\n')
-                    log.flush()
+        time_end = time.time()
+        frames_per_second = 1 / (time_end - time_start)
 
-    output = ('Testing Results: Prec@1 {top1.avg:.3f} Prec@5 {top5.avg:.3f} Loss {loss.avg:.5f}'
-              .format(top1=top1, top5=top5, loss=losses))
-    print(output)
-    if log is not None:
-        log.write(output + '\n')
-        log.flush()
+        img = cv2.resize(img, (640, 480))
+        img = img[:, ::-1]
+        img = add_label(img, gestures.get_name(smooth_prediction), frames_per_second)
+        cv2.imshow(window_name, img)
 
-    if tf_writer is not None:
-        tf_writer.add_scalar('loss/test', losses.avg, epoch)
-        tf_writer.add_scalar('acc/test_top1', top1.avg, epoch)
-        tf_writer.add_scalar('acc/test_top5', top5.avg, epoch)
+        key = cv2.waitKey(1)
+        if is_quit_key(key):  # exit
+            break
+        elif is_fullscreen_key(key):  # full screen
+            full_screen = not full_screen
+            switch_fullscreen_mode(full_screen, window_name)
 
-    return top1.avg
+    cap.release()
+    cv2.destroyAllWindows()
 
-
-def save_checkpoint(state, is_best):
-    filename = '%s/%s/ckpt.pth.tar' % (args.root_model, args.store_name)
-    torch.save(state, filename)
-    if is_best:
-        shutil.copyfile(filename, filename.replace('pth.tar', 'best.pth.tar'))
-
-
-def adjust_learning_rate(optimizer, epoch, lr_type, lr_steps):
-    """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
-    if lr_type == 'step':
-        decay = 0.1 ** (sum(epoch >= np.array(lr_steps)))
-        lr = args.lr * decay
-        decay = args.weight_decay
-    elif lr_type == 'cos':
-        import math
-        lr = 0.5 * args.lr * (1 + math.cos(math.pi * epoch / args.epochs))
-        decay = args.weight_decay
-    else:
-        raise NotImplementedError
-    for param_group in optimizer.param_groups:
-        param_group['lr'] = lr * param_group['lr_mult']
-        param_group['weight_decay'] = decay * param_group['decay_mult']
-
-
-def check_rootfolders():
-    """Create log and model folder"""
-    folders_util = [args.root_log, args.root_model,
-                    os.path.join(args.root_log, args.store_name),
-                    os.path.join(args.root_model, args.store_name)]
-    for folder in folders_util:
-        if not os.path.exists(folder):
-            print('creating folder ' + folder)
-            os.mkdir(folder)
-
-
-if __name__ == '__main__':
-    main()
